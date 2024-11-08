d032c5ee608e5b4ea8343e9b39b84daba25b16fa
Aviv Levinsky <levinskyaviv@gmail.com>
2024-11-07 20:06:08 -0800

    add some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Reward.create!(name: 'Doll', cost: 500, available: true, inventory_amount: 10)
+Reward.create!(name: 'T-shirt', cost: 1000, available: true, inventory_amount: 5)
+Reward.create!(name: 'Water gun', cost: 2000, available: true, inventory_amount: 2)
+Reward.create!(name: 'Magazine', cost: 500, available: false, inventory_amount: 5)
+
+User.create!(name: 'Aviv', email: 'email@domain.com', credits_amount: 5000)
